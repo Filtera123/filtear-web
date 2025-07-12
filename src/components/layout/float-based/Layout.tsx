392789bf08d995e4dad7adf6542fe2cbaa324ff6
queyun123 <3031469073@qq.com>
2025-07-12 10:54:54 +0800

    x | 修改package.json

--- src/components/layout/float-based/Layout.tsx
+++ src/components/layout/float-based/Layout.tsx
@@ -6,7 +6,7 @@ export default function FloatBasedLayout() {
   return (
     <div className="relative flex lg:max-w-4/5 mx-auto min-h-screen">
       {/* 左侧边栏 - 使用sticky定位，允许内部滚动，保持绝对宽度不变 */}
-      <div className="w-[18%] sticky top-0 h-screen overflow-hidden border-gray-200 border-r pr-4">
+      <div className="w-[20%] sticky top-0 h-screen overflow-hidden border-gray-200 border-r pr-4">
         <SideBar />
       </div>
 
@@ -16,7 +16,7 @@ export default function FloatBasedLayout() {
       </main>
 
       {/* 右侧边栏 - 使用sticky定位，允许内部滚动，宽度增加到原来的1.3倍 */}
-      <div className="w-[27%] sticky top-0 h-screen overflow-hidden border-gray-200 border-l pl-4">
+      <div className="w-[25%] sticky top-0 h-screen overflow-hidden border-gray-200 border-l pl-4">
         <RightSideBar />
       </div>
     </div>
